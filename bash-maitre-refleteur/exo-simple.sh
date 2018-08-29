8370c4e78d611e33244b7e370707db6fd4715bcb
Mario <mariofadegnon@gmail.com>
2018-08-29 17:13:27 +0000

    Ajout du test logique encadrement de nb2

--- bash-maitre-refleteur/exo-simple.sh
+++ bash-maitre-refleteur/exo-simple.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+#Déclaration des variables
 nb1=100129901099
 nb2=97
+borne_inf=10
+borne_sup=100
+pos=11
+long=5
+ch1=a1223425672890ABCDEFazertyCDEBILE813-6662666
+
+
 let "multiplication=$nb1*$nb2"
 echo "nb1 multiplié par nb2 égale à $multiplication"
 
 let "modulo=$nb1%$nb2"
 echo "nb1 modulo nb2 égale à $modulo"
+
+
+#Ajout du test d'expression logique
+
+if [ $nb2 -ge $borne_inf -a $nb2 -le $borne_sup ]; #On utilise -a pour faire le et des tests logiques et non &&
+then
+	 echo "vrai";
+else
+	echo "faux";
+fi
+
