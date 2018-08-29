70401c0d22e824303690d28e99821c41467cae10
Mario <mariofadegnon@gmail.com>
2018-08-29 17:00:08 +0000

    nb1 modulo nb2

--- bash-maitre-refleteur/exo-simple.sh
+++ bash-maitre-refleteur/exo-simple.sh
@@ -3,4 +3,7 @@
 nb1=100129901099
 nb2=97
 let "multiplication=$nb1*$nb2"
-echo " nb1 multiplié par nb2 égale à $multiplication"
+echo "nb1 multiplié par nb2 égale à $multiplication"
+
+let "modulo=$nb1%$nb2"
+echo "nb1 modulo nb2 égale à $modulo"
